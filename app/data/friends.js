b878dc2f39ec1b6996bcc9e81bfc9d8864f978e6
jamie-jessi <jamiejudi@gmail.com>
2017-08-13 22:25:44 -0500

    dummy data

---
+++ app/data/friends.js
@@ -0,0 +1,15 @@
+//dummy data
+var friends = [
+    {
+        "name":"Terra",
+        "photo":"https://vignette4.wikia.nocookie.net/finalfantasy/images/b/bd/FFVI_Terra_Branford_Menu_iOS.png/revision/latest?cb=20140219002655",
+        "scores":[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+    },
+    {
+        "name":"Locke",
+        "photo":"https://vignette2.wikia.nocookie.net/finalfantasy/images/8/8b/FFVI_Locke_Cole_Menu_iOS.png/revision/latest?cb=20140219003844",
+        "scores":[5, 2, 4, 4, 5, 1, 2, 5, 4, 1]
+    },
+];
+
+module.exports = friends; //makes file accesible  to other files through require
